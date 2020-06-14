eafec6a9814cf3db84ab2f03f9d1d58fef281a08
koteitan <ukaibutton@mac.com>
2020-06-14 22:19:07 +0900

    implement expand function on the way (up to making parent index matrix)

--- c/bms.c
+++ c/bms.c
@@ -6,8 +6,10 @@
 #define BM_ELEMS_MAX 256
 int main(int argc, char **argv){
   Bm *bm=parse(argv[1]);
-  printbm(bm);
+  Bm *bm1=expand(bm);
+  printbm(bm1);
   free(bm);
+  free(bm1);
   return EXIT_SUCCESS;
 }
 void printbm(Bm *bm){
@@ -80,4 +82,59 @@ Bm* initbm(void){
   Bm *bm=malloc(sizeof(Bm));
   bm->m = malloc(sizeof(Int)*BM_ELEMS_MAX);
   return bm;
+}
+Bm* expand(Bm* bm0){
+  int x,y;
+  Bm *bm1=initbm();
+  int xs=bm0->xs;
+  int ys=bm0->ys;
+  Int *m=bm0->m;
+  Int b =bm0->b;
+  int *p=&m[(xs-1)*ys];
+
+  /* find lowermost non zero */
+  int lnz;
+  for(y=0;y<ys;y++){
+    if(*p++==0){
+      lnz=y-1;
+      break;
+    }
+  }
+  /* simple cut case */
+  if(y==0){ /* child=(0,...,0)  */
+    memcpy(bm1->m, bm0->m, sizeof(Int)*(xs-1)*ys);
+    bm1->xs=xs-1;
+    bm1->ys=ys;
+    bm1->b=b;
+    return bm1;
+  }
+  /* make parent index matrix */
+  int *pim=malloc(sizeof(int)*xs*ys); /* parent index matrix  */
+  for(y=0;y<ys;y++){
+    for(x=0;x<xs;x++){
+      Int c=m[x*ys+y];
+      if(c==0){
+        pim[x*ys+y]=-1;
+        continue;
+      }
+      int px; /* parent index */
+      if(y==0){
+        for(px=x-1;px>=0;px--){
+          if(m[px*ys+y]<c) break;
+        }
+      }else{
+        for(px=pim[x*ys+y-1];px!=-1;px=pim[px*ys+y-1]){
+          if(m[px*ys+y]<c) break;
+        }
+      }
+      pim[x*ys+y]=px;
+#if 1
+      bm1->m[x*ys+y]=(Int)px;
+      bm1->xs=xs;
+      bm1->ys=ys;
+      bm1->b=b;
+#endif
+    }/* for x */
+  }/* for y */
+  return bm1;
 }
