887aa6a7a637b1610f2d640214b3c6481e4ed102
koteitan <ukaibutton@mac.com>
2020-06-27 22:53:39 +0900

    bugfix

--- c/example.sh
+++ c/example.sh
@@ -9,9 +9,9 @@ for i in {6..50}; do
   time ./bms -lrv 2   "(0,0,0)(1,1,1)(2,1,0)" "(0,0,0)(1,1,1)(2,1,0)(1,1,1)" $i >>bm2.0_depth$i.txt
   time ./bms -lrv 3.3 "(0,0,0)(1,1,1)(2,1,0)" "(0,0,0)(1,1,1)(2,1,0)(1,1,1)" $i >>bm3.3_depth$i.txt
   time ./bms -lrv 4   "(0,0,0)(1,1,1)(2,1,0)" "(0,0,0)(1,1,1)(2,1,0)(1,1,1)" $i >>bma.04_depth$i.txt
-  echo "">bm1.1_depth$i.txt
-  echo "">bm2.0_depth$i.txt
-  echo "">bm3.3_depth$i.txt
-  echo "">bm4.0_depth$i.txt
+  echo "">>bm1.1_depth$i.txt
+  echo "">>bm2.0_depth$i.txt
+  echo "">>bm3.3_depth$i.txt
+  echo "">>bm4.0_depth$i.txt
 done
 
